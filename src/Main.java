c84ab177f4a67b8920d788975b348eb622b04cf3
pepoJ34 <schoolpepoo@gmail.com>
2023-05-18 08:27:07 -0300

    Relação de declarar e construir classes, relação com "Pessoa"

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Aula de Orientação à Objetos
+        //Declarar objetos
+        Pessoa adao;
 
+        //Instanciar objetos
+        adao = new Pessoa();
+        Pessoa eva = new Pessoa();
+
+        //Definição do formato
+        adao.nome = "Adão";
+        adao.sobrenome = "Silva";
+
+        eva.nome = "Eva";
+        eva.sobrenome = "Silva";
+
+        //Aciona comportamento
+        adao.falar();
+        eva.falar();
 
 
     }
